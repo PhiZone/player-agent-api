3689a926e18d798c35e70aa7bc5986c15ddfab8d
Naptie <naptie@qq.com>
2025-09-06 18:25:09 +0800

    feat: add support for Amazon S3

--- src/oss/index.ts
+++ src/oss/index.ts
@@ -1,12 +1,14 @@
 import { isLeanCloudInitialized, uploadToLeanCloud } from './leancloud.js';
+import { isS3Initialized, uploadToS3 } from './s3.js';
 
-export const isOSSAvailable = () => isLeanCloudInitialized;
+export const isOSSAvailable = () => isS3Initialized || isLeanCloudInitialized;
 
 export const upload = async (
   name: string,
   buffer: Buffer<ArrayBufferLike>,
   onProgress: (progress: number) => void
 ) =>
+  (await uploadToS3(name, buffer, onProgress)) ||
   (await uploadToLeanCloud(name, buffer, onProgress)) ||
   (() => {
     throw new Error('No OSS provider available');
---
+++ src/oss/s3.ts
@@ -0,0 +1,61 @@
+import config from '../../config.json' with { type: 'json' };
+import { S3Client } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+
+interface S3Config {
+  endpoint: string;
+  region: string;
+  bucket: string;
+  accessKeyId: string;
+  secretAccessKey: string;
+}
+
+let s3: S3Client | undefined = undefined;
+export let isS3Initialized = false;
+
+const s3Config: S3Config | undefined =
+  's3' in config.oss && typeof config.oss.s3 === 'object' ? config.oss.s3 : undefined;
+
+if (s3Config) {
+  s3 = new S3Client({
+    region: s3Config.region,
+    endpoint: s3Config.endpoint,
+    forcePathStyle: true,
+    credentials: {
+      accessKeyId: s3Config.accessKeyId,
+      secretAccessKey: s3Config.secretAccessKey
+    }
+  });
+
+  isS3Initialized = true;
+}
+
+export const uploadToS3 = async (
+  name: string,
+  buffer: Buffer<ArrayBufferLike>,
+  onProgress: (progress: number) => void
+) => {
+  if (!s3Config || !s3) return;
+
+  const upload = new Upload({
+    client: s3,
+    params: {
+      Bucket: s3Config.bucket,
+      Key: name,
+      Body: buffer,
+      ContentType: 'application/octet-stream'
+    }
+  });
+
+  upload.on('httpUploadProgress', (progress) => {
+    if (progress.loaded && progress.total) {
+      const progressPercent = progress.loaded / progress.total;
+      onProgress(progressPercent);
+    }
+  });
+
+  await upload.done();
+  onProgress(1); // Ensure we reach 100%
+
+  return `${s3Config.endpoint}/${s3Config.bucket}/${name}`;
+};
